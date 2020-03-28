21ae11045c14513ee801e6d5609763edb5ec75f1
Carla Holcomb <carla.programmer@gmail.com>
2020-03-28 13:53:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/users/new', to: 'users#new', as: 'register'
+  get '/students', to: 'students#show', as: 'active'
 end
